e9077199fc2ada31549b4a59369b6796006f58b2
Pharaoh Manson <pharaohmanson@gmail.com>
2019-08-19 15:44:29 -0400

    Initial commit

---
+++ Constructors/Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructors
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
+
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public string Author
+        {
+            get { return author; }
+        }
+
+        public int Pages
+        {
+            get { return pages; }
+        }
+    }
+}
---
+++ Constructors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Book book1 = new Book("Harry Potter", "JK Rowling", 456);
+           Book book2 = new Book("Lord of the rings", "Tolkien", 678);
+           Console.WriteLine("{0} by {1}", book1.Title, book1.Author );
+        }
+    }
+}
